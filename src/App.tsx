5b227d59a3667b3d2726ed4d2ec294c19d62feff
David Olowomeye <olowomeyedavid242@gmail.com>
2024-05-13 22:27:16 +0100

    Added Navbar and its responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from "react"
+import NavBar from "./scenes/navbar"
+import { SelectedPage } from "./shared/Types"
+
+
+
 function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home)
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home)
+      }
+      if (window.scrollY !== 0) {
+        setIsTopOfPage(false)
+      }
+    }
+    window.addEventListener("scroll", handleScroll)
+    // When the component unmount
+    return () => window.removeEventListener("scroll", handleScroll)
+  }, [])
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline bg-secondary-500">Testing ff</h1>
-    </>
+    <div className="app text-3xl font-bold ">
+      <NavBar isTopOfPage={isTopOfPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+    </div>
+
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // content: ["./index.html", "./src/**/*.{js,tsx,jsx}"],
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
@@ -8,6 +7,7 @@ module.exports = {
         "gray-20": "#F8F4EB",
         "gray-50": "#EFE6E6",
         "gray-100": "#DFCCCC",
+        "gray-500": "#5E0000",
         "primary-100": "#FFe1E0",
         "primary-300": "#FFA6A3",
         "primary-500": "#FF6B66",
